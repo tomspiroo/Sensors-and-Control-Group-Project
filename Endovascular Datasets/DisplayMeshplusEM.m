130618351755b16ae6c2518bf52fc62c34af5331
jeremyjmansfield-UTS <jeremy.j.mansfield@student.uts.edu.au>
2022-10-25 17:03:06 +1100

    Add Mesh + EM point data
    Add Mesh + EM point data

---
+++ Endovascular Datasets/DisplayMeshplusEM.m
@@ -0,0 +1,31 @@
+clc;
+clear;
+
+load Mesh_Soft_EM_Mod_0601;
+em = [];
+for i = 0:1:2002
+    read = table2array(readtable(string(i) + '.txt','Delimiter',' ','ReadVariableNames',false));
+    for j = 1:1:size(read,2)
+        em(i + 1,j) = read(1,j);
+    end
+end
+
+
+%%
+figure(1);
+trimesh(tri,ver(:,1),ver(:,2),ver(:,3));
+axis equal;
+
+%%
+FV.vertices = ver;
+FV.faces = tri;
+
+figure(2);
+patch(FV,'facecolor',[1 0 0],'facealpha',0.3,'edgecolor','none');
+view(3)
+camlight 
+hold on
+for i = 1:1:size(em,1)
+    scatter3(em(i,1),em(i,2),em(i,3))
+    hold on
+end
