c00f1c9c6ab9a0410fea27697d87b1283776d1d6
Thomas Spiritosanto <thomas.spiritosanto@student.uts.edu.au>
2022-10-25 11:01:04 +1100

    Update GUI_Test.m
    update gui test that allows for 1 initilisation of image (line 5)

--- GUI_Test.m
+++ GUI_Test.m
@@ -1,18 +1,20 @@
 %% GUI Test
 %Open app with handle "GUI"
 gui = srcapp
+%Change number depending on what image you want processed
+ImageNumber = 1200;
 %Change RawImage
-gui.RawImage.ImageSource = '1200.jpg';
+gui.RawImage.ImageSource = strcat(num2str(ImageNumber),'.jpg');
 %OR
-img = imread('1200.jpg');
+img = imread(strcat(num2str(ImageNumber),'.jpg'));
 gui.RawImage.ImageSource = img;
 %Change ProcImage same as above, with handle gui.ProcImage. Centrepoint
 %same as above with gui.CentreImage
-GSI_Masked = imread('1200p.jpg');
+GSI_Masked = imread(strcat(num2str(ImageNumber),'p.jpg'));
 SmallConvert = cat(3, GSI_Masked,GSI_Masked,GSI_Masked);
 gui.ProcImage.ImageSource = SmallConvert;
 
-CentrePointLoc = imread('1200c.jpg');
+CentrePointLoc = imread(strcat(num2str(ImageNumber),'c.jpg'));
 gui.CentreImage.ImageSource = CentrePointLoc;
 %Change rotation value
 gui.RotationOut.Value = 145;
